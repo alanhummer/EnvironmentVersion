af60dbd5f7ccd3caa13f78229151065dc106206b
Hummer, Alan J <Alan.Hummer@landsend.com>
2021-03-29 21:41:14 -0500

    Version update

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ This JS is the main processing set - when DOM loaded, code is fired to prmopt fo
 var config;  //object that will hold all configuration options
 var blnStatusLoaded = false;
 var statusArray;
+var saveResponse;
 
 //And so we begin....
 document.addEventListener('DOMContentLoaded', onDOMContentLoaded, false);
@@ -22,8 +23,11 @@ function onDOMContentLoaded() {
         //Get all of our config parameters
         config = JSON.parse(response); 
 
+        //Now step 1 - load domains
+        step2ProcessURLs();
+
         //Get our statuses first
-        step1LoadStatus();    
+        //step1LoadStatus();    
               
     });
 
@@ -88,34 +92,39 @@ function step1LoadStatus(inputDomain) {
                 //We got it, set the array
                 statusArray = xhr.response;
 
-                //Get the most reacent for each domain
-                statusArray.forEach(function(statusElement1) {
-                    statusArray.forEach(function(statusElement2) {
-                        if (statusElement1.host == statusElement2.host) {
-                            //We hve a match, choose the most recent
-                            if (statusElement1.id == statusElement2.id) {
-                                //Its the same one, skip it
-                            }
-                            else {
-                                var statusDate1 = new Date(statusElement1.insertDate);
-                                var statusDate2 = new Date(statusElement2.insertDate);
-                                if (statusDate2 > statusDate1) {
-                                    //set the most recent
-                                    statusElement2.mostRecent = true;
-                                    statusElement1.mostRecent = false;
+                if (statusArray) {
+
+                    //Get the most reacent for each domain
+                    statusArray.forEach(function(statusElement1) {
+                        statusArray.forEach(function(statusElement2) {
+                            if (statusElement1.host == statusElement2.host) {
+                                //We hve a match, choose the most recent
+                                if (statusElement1.id == statusElement2.id) {
+                                    //Its the same one, skip it
                                 }
                                 else {
-                                    //set the most recent
-                                    statusElement1.mostRecent = true;
-                                    statusElement2.mostRecent = false;                                   
+                                    var statusDate1 = new Date(statusElement1.insertDate);
+                                    var statusDate2 = new Date(statusElement2.insertDate);
+                                    if (statusDate2 > statusDate1) {
+                                        //set the most recent
+                                        statusElement2.mostRecent = true;
+                                        statusElement1.mostRecent = false;
+                                    }
+                                    else {
+                                        //set the most recent
+                                        statusElement1.mostRecent = true;
+                                        statusElement2.mostRecent = false;                                   
+                                    }
                                 }
                             }
-                        }
-                    });               
-                });
+                        });               
+                    });
+
+                }
 
                 //Now step 1 - load domains
                 step2ProcessURLs();
+
             }
             else {
                 //Failed
@@ -231,6 +240,163 @@ function step2ProcessURLs() {
 
 }
 
+//2B for getting the manifest
+function getManifest(inputDomain, docID) {
+
+    var urlToRun = "";
+
+    switch(inputDomain) {
+        case "le-qasa-aws.landsend.com":
+        case "le-devc-aws.landsend.com":
+            urlToRun = config.manifestQA;
+            break;
+        case "origin-aws-useast-1-www.landsend.com":
+        case "origin-aws-uswest-2-www.landsend.com":
+        case "www.landsend.com":
+            urlToRun = config.manifestProd;
+            break;
+        default:
+            urlToRun = config.manifestDev;
+            break;
+    }
+
+    //Try to send
+    try {
+
+        var overrideversion = "";
+        var webversion = "";
+        var tarversion = "";
+        var coversion = "";
+        var feversion = "";
+
+        //Build our object set
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", urlToRun, true);
+
+        //xhr.responseType = 'html';
+        xhr.addEventListener("error", function() {console.log("Manifest Get - Failed - " + urlToRun);});
+        xhr.onload = function() {
+            var status = xhr.status;
+            if (status == 200) {
+                console.log("GOT MANIFEST:" + xhr.response);
+                myManifest = xhr.response;
+                var manifestEntries = xhr.response.split("\n");
+                var outputManifest = "";
+                manifestEntries.forEach(function(manifestEntry) {
+                    if (manifestEntry.includes("current_release_version")) {
+                        if (overrideversion == "") {
+                            overrideversion = manifestEntry.replace("current_release_version: ", "");
+                            overrideversion = overrideversion.replace(/"/g, '');
+                        }
+                        else {
+                            manifestEntry = manifestEntry.replace("{{ current_release_version }}", overrideversion);
+                        }
+                    }
+                    if (manifestEntry.includes("ic_content_display_web_version: ")) {
+                        webversion = manifestEntry.replace("ic_content_display_web_version: ", "");
+                        webversion = webversion.replace(/"/g, '');
+                    }
+                    if (manifestEntry.includes("ic_web_tar_version: ")) {
+                        tarversion = manifestEntry.replace("ic_web_tar_version: ", "");
+                        tarversion = tarversion.replace(/"/g, '');
+                    }
+                    if (manifestEntry.includes("ic_web_fe_secure_checkout_version: ")) {
+                        coversion = manifestEntry.replace("ic_web_fe_secure_checkout_version: ", "");
+                        coversion = coversion.replace(/"/g, '');
+                    }
+                    if (manifestEntry.includes("ic_web_front_end_version: ")) {
+                        feversion = manifestEntry.replace("ic_web_front_end_version: ", "");
+                        feversion = feversion.replace(/"/g, '');
+                    }
+                    outputManifest = outputManifest + manifestEntry + "<br>";
+                });
+                document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("_MANIFEST_", outputManifest);
+
+                console.log("COMPARING AND TESTING: " + saveResponse.versions.ic_web_front_end_version_pdp + " FOR " + feversion);
+ 
+                document.getElementById(inputDomain + '.match.ic_content_display_web_version_product.git.build.version').innerHTML = "<font color='red'>NO MATCH</font>";
+                document.getElementById(inputDomain + '.match.ic_content_display_web_version_basket.git.build.version').innerHTML = "<font color='red'>NO MATCH</font>";
+                document.getElementById(inputDomain + '.match.ic_web_tar_version').innerHTML = "<font color='red'>NO MATCH</font>";
+                document.getElementById(inputDomain + '.match.ic_web_fe_secure_checkout_version').innerHTML = "<font color='red'>NO MATCH</font>";
+                document.getElementById(inputDomain + '.match.ic_web_front_end_version_pdp').innerHTML = "<font color='red'>NO MATCH</font>";
+                document.getElementById(inputDomain + '.match.ic_web_front_end_version_pmp').innerHTML = "<font color='red'>NO MATCH</font>";
+                document.getElementById(inputDomain + '.match.ic_web_front_end_version_basket').innerHTML = "<font color='red'>NO MATCH</font>";
+                document.getElementById(inputDomain + '.match.ic_web_front_end_version_myaccount').innerHTML = "<font color='red'>NO MATCH</font>";
+ 
+                //Now load up our version matching statues
+                if (saveResponse.versions.ic_content_display_web_version_product['git.build.version'].includes(webversion)) {
+                    document.getElementById(inputDomain + '.match.ic_content_display_web_version_product.git.build.version').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_content_display_web_version_product.git.build.version').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+                if (saveResponse.versions.ic_content_display_web_version_basket['git.build.version'].includes(webversion)) {
+                    document.getElementById(inputDomain + '.match.ic_content_display_web_version_basket.git.build.version').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_content_display_web_version_basket.git.build.version').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+                if (saveResponse.versions.ic_web_tar_version.includes(tarversion)) {
+                    document.getElementById(inputDomain + '.match.ic_web_tar_version').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_web_tar_version').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+                if (saveResponse.versions.ic_web_fe_secure_checkout_version.includes(coversion)) {
+                    document.getElementById(inputDomain + '.match.ic_web_fe_secure_checkout_version').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_web_fe_secure_checkout_version').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+                if (saveResponse.versions.ic_web_front_end_version_pdp.includes(feversion)) {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_pdp').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_pdp').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+                if (saveResponse.versions.ic_web_front_end_version_pmp.includes(feversion)) {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_pmp').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_pmp').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+                if (saveResponse.versions.ic_web_front_end_version_basket.includes(feversion)) {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_basket').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_basket').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+                if (saveResponse.versions.ic_web_front_end_version_myaccount.includes(feversion)) {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_myaccount').innerHTML = "<font color='green'>MATCH</font>";                
+                }
+                else {
+                    document.getElementById(inputDomain + '.match.ic_web_front_end_version_myaccount').innerHTML = "<font color='red'>NO MATCH</font>";
+                }
+
+                      /*
+                ic_content_display_web_version: "100.14-SNAPSHOT" = .(2 of these)) match.ic_content_display_web_version_product.git.build.versio
+                ic_web_tar_version: "100.14.0" .match.ic_web_tar_version
+                ic_web_fe_secure_checkout_version: "100.14.0" = match.ic_web_fe_secure_checkout_version
+                ic_web_front_end_version: "100.14.0"   (4 of theise) match.ic_web_front_end_version_myaccount
+
+  */
+                
+            } 
+            else {
+            }
+        };        
+        xhr.send();       
+    }
+    catch(err) {
+        console.log("Manifest Get - Failed - " + urlToRun);
+    }
+
+    return;
+
+}
+
+
+
 //Get Environment info
 function step3GetEnvInfo(inputDomain) {
     
@@ -261,20 +427,19 @@ function step3GetEnvInfo(inputDomain) {
             xhr.addEventListener("error", function() {console.log("Environment Version - Failed - " + inputDomain.domain);});
             xhr.onload = function() {
                 var status = xhr.status;
+                saveResponse = xhr.response;
                 if (status == 200) {
                     //Add it to the output report
                     document.getElementById(detailID).style.display =  'block';
 
                     document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace(/Running.../g, "Done");
 
-
-
-                    document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_product.git.build.com", xhr.response.versions.ic_content_display_web_version_product['git.build.time']).replace("T", " ");
+                    document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_product.git.build.time", xhr.response.versions.ic_content_display_web_version_product['git.build.time']);
                     document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_product.git.build.version", xhr.response.versions.ic_content_display_web_version_product['git.build.version']);
                     document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_product.git.commit.id.abbrev", xhr.response.versions.ic_content_display_web_version_product['git.commit.id.abbrev']);
                     document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_product.git.commit.id.full", xhr.response.versions.ic_content_display_web_version_product['git.commit.id.full']);
 
-                    document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_basket.git.build.com", xhr.response.versions.ic_content_display_web_version_basket['git.build.time'].replace("T", " "));
+                    document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_basket.git.build.time", xhr.response.versions.ic_content_display_web_version_basket['git.build.time']);
                     document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_basket.git.build.version", xhr.response.versions.ic_content_display_web_version_basket['git.build.version']);
                     document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_basket.git.commit.id.abbrev", xhr.response.versions.ic_content_display_web_version_basket['git.commit.id.abbrev']);
                     document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_content_display_web_version_basket.git.commit.id.full", xhr.response.versions.ic_content_display_web_version_basket['git.commit.id.full']);
@@ -319,7 +484,9 @@ function step3GetEnvInfo(inputDomain) {
                         document.getElementById(docID).innerHTML = document.getElementById(docID).innerHTML.replace("versions.ic_web_front_end_version.myaccount", "Error...");
     
                     }
-                 }
+                }
+                getManifest(inputDomain.domain, docID);
+
             };        
             xhr.send();       
         }
